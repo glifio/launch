5ee625c1edac2adb8070f4addd9a147eb20b8eb1
listenaddress <tbobrien612@gmail.com>
2020-10-13 19:17:45 -0400

    Add chain height polling

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,41 @@
 import Nav from '../components/nav'
 import LotusRPCEngine from '@glif/filecoin-rpc-client'
 import { useState, useEffect } from 'react'
-
+let inited = false
 export default function IndexPage() {
   const lotus = new LotusRPCEngine({
     apiAddress: 'https://api.node.glif.io',
   })
-
   const [chainHeight, setChainHeight] = useState('')
+  const mainnetHeight = 148888
+
+  async function init() {
+    const { Height } = await lotus.request('ChainHead')
+    setChainHeight(Height)
+    inited = true
+  }
+  if (!inited) init()
+
   useEffect(() => {
-    async function fetchData() {
-      const { Height } = await lotus.request('ChainHead')
-      console.log('HEAD')
-      setChainHeight(Height)
+    async function poll() {
+      setInterval(async () => {
+        const { Height } = await lotus.request('ChainHead')
+        setChainHeight(Height)
+      }, 10000)
     }
-    fetchData()
+    poll()
   }, [])
 
   return (
     <div>
-      <Nav />
+      {/* <Nav /> */}
       <div className='py-20'>
-        <h1 className='text-5xl text-center text-accent-1'>
-          Filecoin Launch {chainHeight}
-        </h1>
+        {chainHeight && (
+          <h1 className='text-5xl text-center text-accent-1'>
+            Filecoin Mainnet launches on block {chainHeight}
+            <p>{mainnetHeight - chainHeight} blocks to go</p>
+          </h1>
+        )}
       </div>
     </div>
   )
