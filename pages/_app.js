010306dd08ea97ccba9984d71c9402bc167cfc00
listenaddress <tbobrien612@gmail.com>
2020-10-14 13:18:23 -0400

    Update html head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/index.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Glif</title>
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/static/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/static/favicon-32x32.png'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
