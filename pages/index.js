decf0e0ee1ec22895ed28b7dcef4bd8a2ada49c5
listenaddress <tbobrien612@gmail.com>
2020-10-14 10:15:21 -0400

    Add gradient and fonts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Nav from '../components/nav'
 import LotusRPCEngine from '@glif/filecoin-rpc-client'
 import { useState, useEffect } from 'react'
 let inited = false
+
 export default function IndexPage() {
   const lotus = new LotusRPCEngine({
     apiAddress: 'https://api.node.glif.io',
@@ -20,23 +20,51 @@ export default function IndexPage() {
     async function poll() {
       setInterval(async () => {
         const { Height } = await lotus.request('ChainHead')
+        console.log('height', Height)
         setChainHeight(Height)
-      }, 10000)
+      }, 3000)
     }
     poll()
   }, [])
 
   return (
-    <div>
+    <div
+      style={{
+        minHeight: '100vh',
+        backgroundImage:
+          'radial-gradient(circle at 50% -47%, hsl(226.29999999999995, 66%, 20.8%) 39%, hsla(64.69999999999999, 72.6%, 41.6%, 0) 100%), linear-gradient(179deg, hsl(51.5, 89.2%, 78.2%) 23%, #CC6966 100%)',
+      }}
+    >
       {/* <Nav /> */}
-      <div className='py-20'>
-        {chainHeight && (
-          <h1 className='text-5xl text-center text-accent-1'>
-            Filecoin Mainnet launches on block {chainHeight}
-            <p>{mainnetHeight - chainHeight} blocks to go</p>
+      {chainHeight && (
+        <div className='py-20'>
+          <h1
+            className='text-5xl text-center text-accent-1'
+            style={{ color: 'white', fontFamily: 'RT-Alias-Medium' }}
+            // class='animate animate-1'
+          >
+            Filecoin Mainnet launches on block 148,888
+            {/* <Transition
+              from={{ transform: 'translate3d(0,-40px,0)' }}
+              enter={{ transform: 'translate3d(0,0px,0)' }}
+              leave={{ transform: 'translate3d(0,-40px,0)' }}
+            >
+              <div>test</div>
+            </Transition> */}
           </h1>
-        )}
-      </div>
+
+          <h1
+            className='text-5xl text-center text-accent-1'
+            style={{
+              color: 'white',
+              fontFamily: 'RT-Alias-Medium',
+              fontWeight: '700',
+            }}
+          >
+            {mainnetHeight - chainHeight} blocks to go
+          </h1>
+        </div>
+      )}
     </div>
   )
 }
